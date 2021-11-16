1a6ce24a78de85bb4cc865f1781d15324768ba23
ThomasMouchelet <mouchelet.thomas@gmail.com>
2021-11-16 11:15:02 +0100

    fixtures

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -3,12 +3,32 @@
 namespace App\src\Controller;
 
 use App\src\DataFixtures\CustomersFixtures;
+use App\src\Entity\Customer;
+use App\src\Repository\CustomerRepository;
 
 class CustomerController
 {
+    private $customerRepository;
+
+    public function __construct()
+    {
+        $this->customerRepository = new CustomerRepository();
+    }
+
     public function loadFixtures()
     {
         $customersFixtures = new CustomersFixtures();
         $customersFixtures->load();
+    }
+
+    public function newCustomer($post)
+    {
+        $customer = new Customer();
+        $customer
+            ->setEmail($post['email'])
+            ->setAddress($post['address'])
+            ->setCompanyName($post['companyName']);
+
+        $this->customerRepository->addCustomer($customer);
     }
 }
